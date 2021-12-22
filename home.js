73ad4592788f7bf80b5b0026c46018a11a9157f0
lisandroseia <lisandroseia11@gmail.com>
2021-12-22 18:38:49 -0300

    F

--- home.js
+++ home.js
@@ -34,43 +34,43 @@ let trainers = [
         name: 'CESAR MILAN',
         bio: 'Author, public speaker, and TV personality',
         desc: 'Live on TV, and via books and seminars, Cesar has helped millions of dog owners by providing simple take-home tips and solutions to various dog problems. His ‘Dog Whisperer’ program was nominated for an Emmy.',
-            img: '/capstone/assets/images/milan.png'
+            img: './assets/images/milan.png'
     },
     {
         name: 'ZAK GEORGE',
         bio: 'YouTube Star and television personality',
         desc: 'His Dog Training Revolution channel on YouTube is the go-to channel for many who want to bring positive behavioral change in their dogs. He summarizes even the complicated techniques into energy-packed, flashy, concise, and easy-to-understand clips.',
-            img: '/capstone/assets/images/george.jpg'
+            img: './assets/images/george.jpg'
     },
     {
         name: 'PATRICIA MCCONNELL',
         bio: 'Certified Applied Animal Behaviorist',
         desc: 'She co-hosted the ‘Calling All Pets’ show on radio for 14 years (’94-’08) in which she shared her expertise with listeners on animal behavior issues.',
-            img: '/capstone/assets/images/patricia.jpeg'
+            img: './assets/images/patricia.jpeg'
     },
     {
         name: 'SUSAN GARRETT',
         bio: 'Susan specializes in sport, service, and performance dogs',
         desc: 'Susan Garrett’s knowledge on dog agility training is highly sort after in all 6 continents. She has trained and mentored several leading dog trainers and loves sharing her insight via books and podcasts.',
-            img: '/capstone/assets/images/susan.jpg'
+            img: './assets/images/susan.jpg'
     },
     {
         name: 'DAVE SALMONI',
         bio: 'was part of the Tiger ‘rewilding’ project',
         desc: 'His expertise is in rehabilitating broken dogs back into the community, He also shares his DIY guide to living harmoniously with your dog on YouTube and TV shows when invited. His guide is short and straightforward.    ',
-            img: '/capstone/assets/images/dave.jpg'
+            img: './assets/images/dave.jpg'
     },
     {
         name: 'ROGER MUGFORD',
         bio: 'renowned author, inventor, educator, and animal welfare spokesperson.',
         desc: 'As a dog trainer, Dr. Mugford is the real deal. His animal behavior center is where your local vet will refer you to in case of unforgiving pooch problems. He has trained dogs for handicap support service, cancer & Bio detection.    ',
-            img: '/capstone/assets/images/roger.jpg'
+            img: './assets/images/roger.jpg'
     },
     {
         name: 'DOGGY DAN',
         bio: 'Founder and owner of The Online Dog Trainer',
         desc: 'After bouncing from a lot of jobs -math teacher, I.T. sales, British police, etc., he turned to his dream, and over the past decades, he’s been invited to thousands of homes for private one-on-one dog training and consultations.',
-            img: '/capstone/assets/images/dan.jpg'
+            img: './assets/images/dan.jpg'
     }
     ]
     
